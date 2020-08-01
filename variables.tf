bdf22cf098e7cc81a519abefd117bf7fe2b82e70
Ryan <ryan.fee625@gmail.com>
2020-08-01 08:36:41 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,12 @@
-variable "region" {}
+variable "region" {
+us-east4  
+}
 variable "zone" {
   default = "us-east4-a"
 }
 variable "instane_name" {
 description = "Instance Name"  
 }
-variable "instance_type" {}
+variable "instance_type" {
+n1-standard-1  
+}
